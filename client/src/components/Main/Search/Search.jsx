ba0873948c099ec6483a7094d3672f0225c60acd
Cesario87 <ces.albred@gmail.com>
2023-08-17 20:23:09 +0200

    added/fetchAll

--- client/src/components/Main/Search/Search.jsx
+++ client/src/components/Main/Search/Search.jsx
@@ -1,7 +1,33 @@
-// import React from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
-  return <div>Search</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api/?results=100')
+      .then(response => {
+        setUsers(response.data.results);
+      })
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <div>
+        {users.map((user, index) => (
+          <div key={index}>
+            <img src={user.picture.large} alt="User" />
+            <p>Name: {user.name.first} {user.name.last}</p>
+            <hr />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
